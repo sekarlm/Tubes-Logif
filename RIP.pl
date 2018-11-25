7ffe143557022b0c580e5380cbe77c89532dd228
sekarlm <larasatisekar9@gmail.com>
2018-11-25 20:46:11 +0700

    movenemy

--- RIP.pl
+++ RIP.pl
@@ -471,5 +471,5 @@ checkvictory :- enemy_num(0), print('Congratulations, you win the game, you trul
 
 /* Pergerakan musuh */
 movenemy :- move_enemy(voldemort), move_enemy(bellatrix), move_enemy(inheritor), move_enemy(symbiote), move_enemy(madara), move_enemy(obito), move_enemy(sullivan), move_enemy(wazowski), move_enemy(oozmakappa), move_enemy(sousky), !.
-move_enemy(Name) :- at(A,B, Name), random(-1,1,X), C is A + X, C > 0, C < 11, retract(at(_,_,Name)), asserta(at(C,B, Name)), \+ C = A, !. 
-move_enemy(Name) :- at(A,B, Name), random(-1,1,Y), C is B + Y, C > 0, C < 21, retract(at(_,_,Name)), asserta(at(A,C, Name)), !. 
+move_enemy(Name) :- object_loc(A,B, Name), random(-1,1,X), C is A + X, C > 0, C < 16, retract(at(_,_,Name)), asserta(at(C,B, Name)), \+ C = A, !. 
+move_enemy(Name) :- onject_loc(A,B, Name), random(-1,1,Y), C is B + Y, C > 0, C < 16, retract(at(_,_,Name)), asserta(at(A,C, Name)), !. 
